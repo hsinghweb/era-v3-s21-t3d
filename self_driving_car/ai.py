809db0d9aa54214e5ad4d62dc030bc32114ffe5d
Himanshu Singh <himanshu.kumar.singh@gmail.com>
2025-04-05 12:53:56 +0530

    T3D

--- self_driving_car/ai.py
+++ self_driving_car/ai.py
@@ -7,74 +7,158 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 
-class Network(nn.Module):
-    
-    def __init__(self, input_size, nb_action):
-        super(Network, self).__init__()
-        self.input_size = input_size
-        self.nb_action = nb_action
-        self.fc1 = nn.Linear(input_size, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, nb_action)
-    
-    def __repr__(self):
-        return super().__repr__() + f"\nArchitecture:\n- fc1: {self.input_size} -> 128\n- fc2: 128 -> 128\n- fc3: 128 -> {self.nb_action}"
-    
-    def forward(self, state):
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        q_values = self.fc3(x)
-        return q_values
+# Set device for PyTorch operations
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class ReplayMemory(object):
-    
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-    
-    def push(self, event):
-        self.memory.append(event)
-        if len(self.memory) > self.capacity:
-            del self.memory[0]
-    
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+        self.layer_1 = nn.Linear(state_dim, 400)
+        self.layer_2 = nn.Linear(400, 300)
+        self.layer_3 = nn.Linear(300, action_dim)
+        self.max_action = max_action
+
+    def forward(self, x):
+        x = F.relu(self.layer_1(x))
+        x = F.relu(self.layer_2(x))
+        x = self.max_action * torch.tanh(self.layer_3(x))
+        return x
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+        # First Critic network
+        self.layer_1 = nn.Linear(state_dim + action_dim, 400)
+        self.layer_2 = nn.Linear(400, 300)
+        self.layer_3 = nn.Linear(300, 1)
+        # Second Critic network
+        self.layer_4 = nn.Linear(state_dim + action_dim, 400)
+        self.layer_5 = nn.Linear(400, 300)
+        self.layer_6 = nn.Linear(300, 1)
+
+    def forward(self, x, u):
+        xu = torch.cat([x, u], 1)
+        # Forward-Propagation on the first Critic Neural Network
+        x1 = F.relu(self.layer_1(xu))
+        x1 = F.relu(self.layer_2(x1))
+        x1 = self.layer_3(x1)
+        # Forward-Propagation on the second Critic Neural Network
+        x2 = F.relu(self.layer_4(xu))
+        x2 = F.relu(self.layer_5(x2))
+        x2 = self.layer_6(x2)
+        return x1, x2
+
+    def Q1(self, x, u):
+        xu = torch.cat([x, u], 1)
+        x1 = F.relu(self.layer_1(xu))
+        x1 = F.relu(self.layer_2(x1))
+        x1 = self.layer_3(x1)
+        return x1
+
+class ReplayBuffer(object):
+    def __init__(self, max_size=1e6):
+        self.storage = []
+        self.max_size = max_size
+        self.ptr = 0
+
+    def add(self, transition):
+        if len(self.storage) == self.max_size:
+            self.storage[int(self.ptr)] = transition
+            self.ptr = (self.ptr + 1) % self.max_size
+        else:
+            self.storage.append(transition)
+            self.ptr = (self.ptr + 1) % self.max_size
+
     def sample(self, batch_size):
-        samples = zip(*random.sample(self.memory, batch_size))
-        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+        ind = np.random.randint(0, len(self.storage), size=batch_size)
+        batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = [], [], [], [], []
+        for i in ind:
+            state, next_state, action, reward, done = self.storage[i]
+            batch_states.append(np.array(state, copy=False))
+            batch_next_states.append(np.array(next_state, copy=False))
+            batch_actions.append(np.array(action, copy=False))
+            batch_rewards.append(np.array(reward, copy=False))
+            batch_dones.append(np.array(done, copy=False))
+        return np.array(batch_states), np.array(batch_next_states), np.array(batch_actions), np.array(batch_rewards).reshape(-1, 1), np.array(batch_dones).reshape(-1, 1)
 
-class Dqn():
+class TD3(object):
     
-    def __init__(self, input_size, nb_action, gamma):
-        self.gamma = gamma
+    def __init__(self, state_dim, action_dim, max_action):
+        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target.load_state_dict(self.actor.state_dict())
+        self.actor_optimizer = optim.Adam(self.actor.parameters())
+        self.critic = Critic(state_dim, action_dim).to(device)
+        self.critic_target = Critic(state_dim, action_dim).to(device)
+        self.critic_target.load_state_dict(self.critic.state_dict())
+        self.critic_optimizer = optim.Adam(self.critic.parameters())
+        self.max_action = max_action
         self.reward_window = []
-        self.model = Network(input_size, nb_action)
-        self.memory = ReplayMemory(100000)
-        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001)
-        self.last_state = torch.Tensor(input_size).unsqueeze(0)
-        self.last_action = 0
+        self.memory = ReplayBuffer(100000)
+        self.last_state = torch.Tensor(state_dim).unsqueeze(0)
+        self.last_action = np.zeros(action_dim)
         self.last_reward = 0
-    
+
     def select_action(self, state):
-        with torch.no_grad():
-            probs = F.softmax(self.model(state)*100, dim=1) # T=100
-            action = probs.multinomial(1)
-            return action.data[0,0]
-    
-    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
-        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
-        next_outputs = self.model(batch_next_state).detach().max(1)[0]
-        target = self.gamma*next_outputs + batch_reward
-        td_loss = F.smooth_l1_loss(outputs, target)
-        self.optimizer.zero_grad()
-        td_loss.backward(retain_graph = True)
-        self.optimizer.step()
-    
+        state = torch.Tensor(state.reshape(1, -1)).to(device)
+        return self.actor(state).cpu().data.numpy().flatten()
+
+    def train(self, replay_buffer, iterations, batch_size=100, discount=0.99, tau=0.005, policy_noise=0.2, noise_clip=0.5, policy_freq=2):
+        for it in range(iterations):
+            # Sample a batch of transitions from replay buffer
+            batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = replay_buffer.sample(batch_size)
+            state = torch.Tensor(batch_states).to(device)
+            next_state = torch.Tensor(batch_next_states).to(device)
+            action = torch.Tensor(batch_actions).to(device)
+            reward = torch.Tensor(batch_rewards).to(device)
+            done = torch.Tensor(batch_dones).to(device)
+
+            # Select next action according to target policy
+            next_action = self.actor_target(next_state)
+            
+            # Add noise to next action
+            noise = torch.Tensor(batch_actions).data.normal_(0, policy_noise).to(device)
+            noise = noise.clamp(-noise_clip, noise_clip)
+            next_action = (next_action + noise).clamp(-self.max_action, self.max_action)
+
+            # Compute target Q-values
+            target_Q1, target_Q2 = self.critic_target(next_state, next_action)
+            target_Q = torch.min(target_Q1, target_Q2)
+            target_Q = reward + ((1 - done) * discount * target_Q).detach()
+
+            # Compute current Q-values
+            current_Q1, current_Q2 = self.critic(state, action)
+
+            # Compute critic loss
+            critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+
+            # Optimize the critics
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic_optimizer.step()
+
+            # Delayed policy updates
+            if it % policy_freq == 0:
+                # Compute actor loss
+                actor_loss = -self.critic.Q1(state, self.actor(state)).mean()
+                
+                # Optimize the actor
+                self.actor_optimizer.zero_grad()
+                actor_loss.backward()
+                self.actor_optimizer.step()
+
+                # Update target networks
+                for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+                    target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+                for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+                    target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
     def update(self, reward, new_signal):
         new_state = torch.Tensor(new_signal).float().unsqueeze(0)
-        self.memory.push((self.last_state, new_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
+        self.memory.add((self.last_state.numpy().flatten(), new_state.numpy().flatten(), self.last_action, reward, False))
         action = self.select_action(new_state)
-        if len(self.memory.memory) > 500:
-            batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(500)
-            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
+        if len(self.memory.storage) > 500:
+            self.train(self.memory, 1, batch_size=500)
         self.last_action = action
         self.last_state = new_state
         self.last_reward = reward
@@ -82,21 +166,30 @@ class Dqn():
         if len(self.reward_window) > 1000:
             del self.reward_window[0]
         return action
-    
+
     def score(self):
         return sum(self.reward_window)/(len(self.reward_window)+1.)
-    
+
     def save(self):
-        torch.save({'state_dict': self.model.state_dict(),
-                    'optimizer' : self.optimizer.state_dict(),
-                   }, 'model.pth')
-    
+        torch.save({
+            'actor_state_dict': self.actor.state_dict(),
+            'actor_target_state_dict': self.actor_target.state_dict(),
+            'critic_state_dict': self.critic.state_dict(),
+            'critic_target_state_dict': self.critic_target.state_dict(),
+            'actor_optimizer': self.actor_optimizer.state_dict(),
+            'critic_optimizer': self.critic_optimizer.state_dict()
+        }, 'td3_model.pth')
+
     def load(self):
-        if os.path.isfile('model.pth'):
+        if os.path.isfile('td3_model.pth'):
             print("=> loading checkpoint... ")
-            checkpoint = torch.load('model.pth')
-            self.model.load_state_dict(checkpoint['state_dict'])
-            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            checkpoint = torch.load('td3_model.pth')
+            self.actor.load_state_dict(checkpoint['actor_state_dict'])
+            self.actor_target.load_state_dict(checkpoint['actor_target_state_dict'])
+            self.critic.load_state_dict(checkpoint['critic_state_dict'])
+            self.critic_target.load_state_dict(checkpoint['critic_target_state_dict'])
+            self.actor_optimizer.load_state_dict(checkpoint['actor_optimizer'])
+            self.critic_optimizer.load_state_dict(checkpoint['critic_optimizer'])
             print("done !")
         else:
             print("no checkpoint found...")
--- self_driving_car/map.py
+++ self_driving_car/map.py
@@ -32,8 +32,8 @@ from kivy.core.image import Image as CoreImage
 from PIL import Image as PILImage
 
 
-from ai import Dqn
-
+from ai import TD3
+import torch
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
@@ -50,9 +50,9 @@ n_points = 0
 
 length = 0
 
-model = Dqn(5,3,0.9)
-
-action2rotation = [0,5,-5]
+# Initialize TD3 with continuous action space
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = TD3(state_dim=5, action_dim=1, max_action=5.0)  # Single continuous action for rotation
 
 last_reward = 0
 
@@ -155,15 +155,14 @@ class Car(Widget):
 
 
     def move(self, rotation):
-
         self.pos = Vector(*self.velocity) + self.pos
 
         # Clamp car position immediately after movement
-
         self.x = min(max(self.x, 10), longueur - 10)
-
         self.y = min(max(self.y, 10), largeur - 10)
-        self.rotation = rotation
+        
+        # Convert continuous action to rotation
+        self.rotation = float(rotation)  # Direct continuous control
 
         self.angle = self.angle + self.rotation
 
@@ -245,28 +244,18 @@ class Ball3(Widget):
 
 class Game(Widget):
 
-
     car = ObjectProperty(None)
-
     ball1 = ObjectProperty(None)
-
     ball2 = ObjectProperty(None)
-
     ball3 = ObjectProperty(None)
 
-
     def serve_car(self):
-
         self.car.pos = (822,452)
-
         self.car.velocity = Vector(6, 0)
 
-
     def update(self, dt):
-
         global model
         global last_reward
-
         global scores
         global last_distance
         global goal_x
@@ -274,117 +263,83 @@ class Game(Widget):
         global longueur
         global largeur
         global swap
-        
 
         longueur = int(Config.get('graphics', 'width'))
-
         largeur = int(Config.get('graphics', 'height'))
 
         if first_update:
             init()
 
-
         # Ensure car position stays within bounds
-
         car_x = min(max(int(self.car.x), 10), longueur - 10)
-
         car_y = min(max(int(self.car.y), 10), largeur - 10)
         
-
         # Update car position to stay within bounds
-
         self.car.x = car_x
         self.car.y = car_y
 
-
+        # Calculate state inputs
         xx = goal_x - car_x
-
         yy = goal_y - car_y
-
         orientation = Vector(*self.car.velocity).angle((xx,yy))/180.
-
         last_signal = [self.car.signal1, self.car.signal2, self.car.signal3, orientation, -orientation]
+        
+        # Get continuous action from TD3
         action = model.update(last_reward, last_signal)
         scores.append(model.score())
-
-        rotation = action2rotation[action]
-
-        self.car.move(rotation)
-
+        
+        # Apply continuous rotation directly
+        self.car.move(action[0])  # Use first dimension of action as rotation
+        
         distance = np.sqrt((car_x - goal_x)**2 + (car_y - goal_y)**2)
-
         self.ball1.pos = self.car.sensor1
-
         self.ball2.pos = self.car.sensor2
-
         self.ball3.pos = self.car.sensor3
 
-
-        # Ensure coordinates are within bounds before accessing sand array
-
+        # Reward shaping for continuous control
         if 0 <= car_x < longueur and 0 <= car_y < largeur and sand[car_x, car_y] > 0:
-
             self.car.velocity = Vector(0.5, 0).rotate(self.car.angle)
-
             print(1, goal_x, goal_y, distance, car_x, car_y, im.read_pixel(car_x, car_y))
-
-            last_reward = -1
-
+            last_reward = -2.0  # Stronger penalty for hitting sand
         else:
-
             self.car.velocity = Vector(2, 0).rotate(self.car.angle)
-
-            last_reward = -0.15
-
+            # Continuous reward based on distance improvement
+            distance_improvement = last_distance - distance
+            last_reward = distance_improvement * 0.5  # Scale the reward
+            
             print(0, goal_x, goal_y, distance, car_x, car_y, im.read_pixel(car_x, car_y))
-
-            if distance < last_distance:
-
-                last_reward = 0.1
-
+            
+            # Additional reward for staying on road
+            last_reward += 0.1
+            
+            # Penalty for excessive rotation
+            rotation_penalty = -0.1 * abs(action[0]) / model.max_action
+            last_reward += rotation_penalty
 
         if self.car.x < 10:
-
             self.car.x = 10
-
-            last_reward = -1
-
+            last_reward = -2.0
         if self.car.x > self.width - 10:
-
             self.car.x = self.width - 10
-
-            last_reward = -1
-
+            last_reward = -2.0
         if self.car.y < 5:
-
             self.car.y = 5
-
-            last_reward = -1
-
+            last_reward = -2.0
         if self.car.y > self.height - 5:
-
             self.car.y = self.height - 5
-
-            last_reward = -1
-
+            last_reward = -2.0
 
         if distance < 25:
-
             if swap == 1:
-
                 goal_x = 1000
-
                 goal_y = 520
-
                 swap = 0
-
             else:
-
                 goal_x = 1040
-
                 goal_y = 158
-
                 swap = 1
+            last_reward = 10.0  # Bonus for reaching goal
+            
         last_distance = distance
 
 
